2491c6de0115076d0290776443bda8180c755a34
Lorena Ang <45380244+lorena-ang@users.noreply.github.com>
2021-05-05 21:20:41 -0500

    Juego terminado

--- Grupo1_LorenaAng_A01039980/Assets/Scripts/GameOver.cs
+++ Grupo1_LorenaAng_A01039980/Assets/Scripts/GameOver.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
     public void Setup() 
     {
         gameObject.SetActive(true);
+    }
+
+    public void RestartButton()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
     }
 }
